a8538cd33181f83cc970f957fff471aa8543a9d5
Felipe Alves <felipeaamodio@gmail.com>
2022-10-31 20:28:54 -0300

    done

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect, useRef } from 'react';
-import { Image, SafeAreaView, ScrollView, TextInput, View } from 'react-native';
+import { Image, SafeAreaView, ScrollView, TextInput, TouchableOpacity, View, Text } from 'react-native';
+
 import {Camera, CameraType} from 'expo-camera';
+import {captureRef} from 'react-native-view-shot'; //capturar a screenshot
+import * as Sharing from 'expo-sharing';
 
 import { Header } from '../components/Header';
 import { Button } from '../components/Button';
@@ -10,14 +13,21 @@ import { styles } from './styles';
 import { POSITIONS, PositionProps } from '../utils/positions';
 
 export function Home() {
+  const [photo, setPhotoURI] = useState<null | string>(null);
   const [positionSelected, setPositionSelected] = useState<PositionProps>(POSITIONS[0]);
   const [hasCameraPermission, setHasCameraPermission] = useState(false);
 
-  const cameraRef = useRef<Camera>(null) //anotar a referência da câmera
+  const cameraRef = useRef<Camera>(null); //anotar a referência da câmera
+  const screenShotRef = useRef(null); //mostrar a ref da area q vai ser capturada
 
   async function handleTakePicture(){
     const photo = await cameraRef.current.takePictureAsync();
-    console.log(photo);
+    setPhotoURI(photo.uri)
+  }
+
+  async function shareScreenShot(){
+    const screenshot = await captureRef(screenShotRef);
+    await Sharing.shareAsync('file://' + screenshot);
   }
 
   useEffect(() => {
@@ -28,20 +38,24 @@ export function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scroll} showsVerticalScrollIndicator={false}>
-        <View>
+        <View ref={screenShotRef} style={styles.sticker}>
           <Header position={positionSelected} />
 
           <View style={styles.picture}>
 
            {
-            hasCameraPermission ? 
+            hasCameraPermission && !photo ? 
             <Camera
               ref={cameraRef} 
               style={styles.camera}
               type={CameraType.front}
             /> 
             :
-            <Image source={{ uri: 'https://filestore.community.support.microsoft.com/api/images/354ad963-3130-4924-b870-71a46b54fb4a?upload=true' }} style={styles.camera} />
+            <Image 
+            source={{ uri: photo ? photo : 'https://filestore.community.support.microsoft.com/api/images/354ad963-3130-4924-b870-71a46b54fb4a?upload=true' }} 
+            style={styles.camera}
+            onLoad={shareScreenShot} //garantir q a imagem foi carregada
+            />
            }
 
             <View style={styles.player}>
@@ -57,6 +71,12 @@ export function Home() {
           onChangePosition={setPositionSelected}
           positionSelected={positionSelected}
         />
+
+        <TouchableOpacity onPress={() => setPhotoURI(false)}>
+          <Text style={styles.retry}>
+            Nova foto
+          </Text>
+        </TouchableOpacity>
 
         <Button title="Compartilhar" onPress={handleTakePicture} />
       </ScrollView>
